ac6f44c02c07e05092226b82bfb0afab199dd88a
dvpermyakov <dvpermyakov1@gmail.com>
2020-05-18 23:40:43 +0300

    fix

--- processor/build.gradle.kts
+++ processor/build.gradle.kts
@@ -15,6 +15,7 @@ dependencies {
     implementation(kotlin("stdlib"))
     implementation(project(":core"))
     implementation("javax.inject:javax.inject:1")
+    implementation("javax.annotation:javax.annotation-api:1.3.2")
     implementation("com.squareup:kotlinpoet:1.5.0")
     implementation("com.google.auto.service:auto-service:1.0-rc4")
     kapt("com.google.auto.service:auto-service:1.0-rc4")
