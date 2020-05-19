f16708a400c054011a21e3c6146f08fe1d759f39
dvpermyakov <dvpermyakov1@gmail.com>
2020-05-19 18:19:27 +0300

    remove MainComponentFactory.kt

--- sample/src/main/java/com/dvpermyakov/dagger/sample/di/MainComponentFactory.kt
+++
@@ -1,7 +0,0 @@
-package com.dvpermyakov.dagger.sample.di
-
-import KDaggerMainComponent
-
-object MainComponentFactory {
-    fun create(module: MainModule): MainComponent = KDaggerMainComponent(mainModule = module)
-}
