f3bf1299eac97481f2cf86f9d0182d7af82dd307
dvpermyakov <dvpermyakov1@gmail.com>
2020-05-26 14:51:32 +0300

    add sample module with binds

--- processor/src/test/java/com/dvpermyakov/dagger/graph/ComponentGraphTraversingTest.kt
+++ processor/src/test/java/com/dvpermyakov/dagger/graph/ComponentGraphTraversingTest.kt
@@ -1,11 +1,8 @@
 package com.dvpermyakov.dagger.graph
 
-import com.dvpermyakov.dagger.sample.EmptyModule
-import com.dvpermyakov.dagger.sample.SampleData
-import com.dvpermyakov.dagger.sample.SampleModule
+import com.dvpermyakov.dagger.sample.*
 import com.dvpermyakov.dagger.utils.MockProcessingEnvironment
 import com.dvpermyakov.dagger.utils.className.toClassName
-import com.dvpermyakov.dagger.utils.element.getMethodElements
 import com.google.testing.compile.CompilationRule
 import com.squareup.kotlinpoet.ClassName
 import org.junit.Assert
@@ -37,9 +34,6 @@ class ComponentGraphTraversingTest {
     fun emptyModule() {
         val moduleElement = processingEnvironment.elementUtils.getTypeElement(EmptyModule::class.java.name)
         graph.setModules(listOf(moduleElement))
-        moduleElement.getMethodElements().forEach { executableElement ->
-            graph.addElementInModule(executableElement, moduleElement)
-        }
         Assert.assertEquals(emptyList<ClassName>(), graph.getClassNames())
     }
 
@@ -47,9 +41,13 @@ class ComponentGraphTraversingTest {
     fun sampleModule() {
         val moduleElement = processingEnvironment.elementUtils.getTypeElement(SampleModule::class.java.name)
         graph.setModules(listOf(moduleElement))
-        moduleElement.getMethodElements().forEach { executableElement ->
-            graph.addElementInModule(executableElement, moduleElement)
-        }
         Assert.assertEquals(listOf(SampleData::class.java.toClassName()), graph.getClassNames())
+    }
+
+    @Test
+    fun sampleModuleWithBinds() {
+        val moduleElement = processingEnvironment.elementUtils.getTypeElement(SampleModuleWithBinds::class.java.name)
+        graph.setModules(listOf(moduleElement))
+        Assert.assertEquals(listOf(SampleInterface::class.java.toClassName()), graph.getClassNames())
     }
 }
