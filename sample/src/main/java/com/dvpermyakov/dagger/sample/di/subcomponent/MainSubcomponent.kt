42a868914bc3e002408c41ddbf455dfe142ecffd
dvpermyakov <dvpermyakov1@gmail.com>
2020-05-26 14:49:05 +0300

    add parameter module

--- sample/src/main/java/com/dvpermyakov/dagger/sample/di/subcomponent/MainSubcomponent.kt
+++ sample/src/main/java/com/dvpermyakov/dagger/sample/di/subcomponent/MainSubcomponent.kt
@@ -1,9 +1,13 @@
 package com.dvpermyakov.dagger.sample.di.subcomponent
 
 import com.dvpermyakov.dagger.annotation.Subcomponent
+import com.dvpermyakov.dagger.sample.di.modules.ParameterModule
+import com.dvpermyakov.dagger.sample.domain.ParameterConfig
 import com.dvpermyakov.dagger.sample.presentation.TransactionViewModel
 
-@Subcomponent(modules = [])
+@Subcomponent(modules = [ParameterModule::class])
 interface MainSubcomponent {
     fun getTransactionViewModel(): TransactionViewModel
+
+    fun getParameterConfig(): ParameterConfig
 }
